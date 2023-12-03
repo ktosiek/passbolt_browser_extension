71f26a0705fae0702e6aa7f0dc3f347fa65a9a03
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2023-12-03 19:01:18 +0100

    Build with dream2nix

---
+++ default.nix
@@ -0,0 +1,41 @@
+{ sources ? import ./nix/sources.nix
+, dream2nix ? import sources.dream2nix
+, pkgs ? import sources.nixpkgs { overlays = [] ; config = {}; }
+}:
+dream2nix.lib.evalModules {
+  packageSets.nixpkgs = pkgs;
+  modules = [ ({ lib, config, dream2nix, ... }:
+    let packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+    in 
+  {
+    inherit (packageJson) name version; 
+
+    imports = [
+      dream2nix.modules.dream2nix.nodejs-package-lock
+      dream2nix.modules.dream2nix.nodejs-granular
+    ];
+
+    nodejs-package-lock = {
+      source = ./.;
+    };
+
+    nodejs-granular = {
+      buildScript = ''
+        patchShebangs --build node_modules/*/bin/
+        grunt build-firefox
+      '';
+    };
+
+    mkDerivation = {
+      src = lib.cleanSource ./.;
+      # nativeBuildInputs = [ pkgs.breakpointHook ];
+
+      shellHook = ''
+        PATH=${builtins.toString ./.}/node_modules/.bin:$PATH;
+
+        echo "Build: grunt build-firefox  # generuje dist/firefox/passbolt-*.zip"
+      '';
+    };
+
+  }) ];
+}
