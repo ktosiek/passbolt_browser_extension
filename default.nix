059ccec711859b42a1cc82eff66be0ea01969adb
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2024-04-14 16:06:47 +0200

    Automaticaly running `npm install` in dev shell

--- default.nix
+++ default.nix
@@ -1,41 +1,48 @@
 { sources ? import ./nix/sources.nix
 , dream2nix ? import sources.dream2nix
-, pkgs ? import sources.nixpkgs { overlays = [] ; config = {}; }
+, pkgs ? import sources.nixpkgs { overlays = [ ]; config = { }; }
 }:
 dream2nix.lib.evalModules {
   packageSets.nixpkgs = pkgs;
-  modules = [ ({ lib, config, dream2nix, ... }:
-    let packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-    in 
-  {
-    inherit (packageJson) name version; 
+  modules = [
+    ({ lib, config, dream2nix, ... }:
+      let packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+      in
+      {
+        inherit (packageJson) name version;
 
-    imports = [
-      dream2nix.modules.dream2nix.nodejs-package-lock
-      dream2nix.modules.dream2nix.nodejs-granular
-    ];
+        imports = [
+          dream2nix.modules.dream2nix.nodejs-package-lock
+          dream2nix.modules.dream2nix.nodejs-granular
+        ];
 
-    nodejs-package-lock = {
-      source = ./.;
-    };
+        nodejs-package-lock = {
+          source = ./.;
+        };
 
-    nodejs-granular = {
-      buildScript = ''
-        patchShebangs --build node_modules/*/bin/
-        grunt build-firefox
-      '';
-    };
+        nodejs-granular = {
+          buildScript = ''
+            patchShebangs --build node_modules/*/bin/
+            grunt build-firefox
+          '';
+        };
 
-    mkDerivation = {
-      src = lib.cleanSource ./.;
-      # nativeBuildInputs = [ pkgs.breakpointHook ];
+        mkDerivation = {
+          src = lib.cleanSource ./.;
+          # nativeBuildInputs = [ pkgs.breakpointHook ];
 
-      shellHook = ''
-        PATH=${builtins.toString ./.}/node_modules/.bin:$PATH;
+          shellHook = ''
+            PATH=${builtins.toString ./.}/node_modules/.bin:$PATH;
 
-        echo "Build: grunt build-firefox  # generuje dist/firefox/passbolt-*.zip"
-      '';
-    };
+            if [ "$(cat package-lock.json)" != "$(cat node_modules/_package-lock.json 2>/dev/null)" ]; then
+              npm install && \
+              cp package-lock.json node_modules/_package-lock.json
+            fi
 
-  }) ];
+            echo "Build: grunt build-firefox  # generuje dist/firefox/passbolt-*.zip"
+          '';
+        };
+
+      })
+  ];
 }
